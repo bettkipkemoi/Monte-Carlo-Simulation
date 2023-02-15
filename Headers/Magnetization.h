3ecf87898c36e6365cad48131231170c2a19e8a7
Bett Kipkemoi <121342739+bettkipkemoi@users.noreply.github.com>
2023-02-14 17:27:39 -0800

    Headers

---
+++ Headers/Magnetization.h
@@ -0,0 +1,50 @@
+double Magnetization(int lattice[16][16][16])
+{
+
+/* Initialize local variables */
+int i, j, k, len = 16;
+double magnetization = 0, nmax = 0;
+double n0 = 0, n1 = 0, n2 = 0;
+double L3 = 16*16*16;
+
+/* Walk over lattice to check state */
+for (i = 1; i <= len; i++)
+{
+    for (j = 1; j <= len; j++)
+    {
+        for (k = 1; k <= len; k++)
+        {
+        	if (lattice[i-1][j-1][k-1] == 0)
+        	{
+        		n0 = n0 + 1;
+        	}
+
+        	if (lattice[i-1][j-1][k-1] == 1)
+        	{
+        		n1 = n1 + 1;
+        	}
+
+        	if (lattice[i-1][j-1][k-1] == 2)
+        	{
+        		n2 = n2 + 1;
+        	}
+        }
+    }
+}
+
+nmax = n0;
+
+if (n1 > nmax)
+{
+	nmax = n1;
+}
+
+if (n2 > nmax)
+{
+	nmax = n2;
+}
+
+magnetization = (nmax-(L3-nmax)/2);
+
+return magnetization;
+}
